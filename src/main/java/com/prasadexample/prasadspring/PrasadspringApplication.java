6a2b6b73c1756d5eee0b23e8a2321cc0ef828790
Prasad <prasad.da@aol.com>
2024-03-16 08:58:09 -0700

    na

---
+++ src/main/java/com/prasadexample/prasadspring/PrasadspringApplication.java
@@ -0,0 +1,13 @@
+package com.prasadexample.prasadspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrasadspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrasadspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasadexample/prasadspring/PrasadspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasadexample.prasadspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrasadspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
